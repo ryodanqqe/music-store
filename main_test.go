7c41e1eb24c0d79f0e21908f238b079822b91e31
Yerzhan <omnilumiere@gmail.com>
2023-07-20 05:14:50 +0600

    add new read api functions

--- main_test.go
+++ main_test.go
@@ -7,35 +7,97 @@ import (
 	"testing"
 )
 
-func TestGetAlbums(t *testing.T) {
-	rPath := "/albums"
+func requestHandler(w *httptest.ResponseRecorder, req *http.Request) {
 	router := getRouter()
-	req, _ := http.NewRequest("GET", rPath, strings.NewReader(""))
-	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
+}
+
+func TestGetAlbums(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/albums", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
 	if w.Code != http.StatusOK {
-		t.Fatal("status not 200")
+		t.Fatal("status must be 200")
+	}
+}
+
+func TestPostAlbumCorrectStructure(t *testing.T) {
+	req, _ := http.NewRequest("POST", "/albums", strings.NewReader(`{"title": "test"}`))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusCreated {
+		t.Fatal("status must be 201", w.Code)
+	}
+}
+
+func TestPostAlbumBadStructure(t *testing.T) {
+	req, _ := http.NewRequest("POST", "/albums", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatal("status must be 400", w.Code)
 	}
 }
 
 func TestAlbumDetail(t *testing.T) {
-	rPath := "/albums/"
-	router := getRouter()
-	req, _ := http.NewRequest("GET", rPath+"1", strings.NewReader(""))
+	req, _ := http.NewRequest("GET", "/albums/1", strings.NewReader(""))
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+	requestHandler(w, req)
+
 	if w.Code != http.StatusOK {
-		t.Fatal("status not 200")
+		t.Fatal("status must be 200")
 	}
 }
 
 func TestAlbumNotFound(t *testing.T) {
-	rPath := "/albums/"
-	router := getRouter()
-	req, _ := http.NewRequest("GET", rPath+"100", strings.NewReader(""))
+	req, _ := http.NewRequest("GET", "/albums/1000", strings.NewReader(""))
 	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+	requestHandler(w, req)
+
 	if w.Code != http.StatusNotFound {
-		t.Fatal("status not 404")
+		t.Fatal("status must be 404")
+	}
+}
+
+func TestDeleteAlbum(t *testing.T) {
+	req, _ := http.NewRequest("DELETE", "/albums/1", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusNoContent {
+		t.Fatal("status must be 204")
+	}
+}
+
+func TestDeleteAlbumNotFound(t *testing.T) {
+	req, _ := http.NewRequest("DELETE", "/albums/1000", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusNotFound {
+		t.Fatal("status must be 404")
+	}
+}
+
+func TestUpdateAlbum(t *testing.T) {
+	req, _ := http.NewRequest("PUT", "/albums/2", strings.NewReader(`{"title": "test"}`))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatal("status must be 200", w.Code)
+	}
+}
+
+func TestUpdateAlbumNotFound(t *testing.T) {
+	req, _ := http.NewRequest("PUT", "/albums/1000", strings.NewReader(""))
+	w := httptest.NewRecorder()
+	requestHandler(w, req)
+
+	if w.Code != http.StatusNotFound {
+		t.Fatal("status must be 404")
 	}
 }
