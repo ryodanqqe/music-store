b37e6eae405ccd337040e809a40e6707e3b2162f
Yerzhan <omnilumiere@gmail.com>
2023-07-20 07:45:43 +0600

    change data storage implementation

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var storage = NewStorage()
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -13,13 +15,13 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+func main() {
+	router := getRouter()
+	router.Run("localhost:8080")
 }
 
 func getRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
@@ -29,60 +31,48 @@ func getRouter() *gin.Engine {
 	return router
 }
 
-func main() {
-	router := getRouter()
-	router.Run("localhost:8080")
-}
-
 func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	c.IndentedJSON(http.StatusOK, storage.Read())
 }
 
 func postAlbums(c *gin.Context) {
 	var newAlbum album
-
 	if err := c.BindJSON(&newAlbum); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "bad request"})
+		return
 	}
-
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, albums)
+	storage.Create(newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func getAlbumByID(c *gin.Context) {
 	id := c.Param("id")
-
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	album, err := storage.ReadOne(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
-}
-
-func deleteAlbumsByID(c *gin.Context) {
-	id := c.Param("id")
-
-	for i, a := range albums {
-		if a.ID == id {
-			albums = append(albums[:i], albums[i+1:]...)
-			c.IndentedJSON(http.StatusNoContent, a)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusOK, album)
 }
 
 func updateAlbumsByID(c *gin.Context) {
 	id := c.Param("id")
-
-	for i, a := range albums {
-		if a.ID == id {
-			c.BindJSON(&albums[i])
-			c.IndentedJSON(http.StatusOK, albums[i])
-			return
-		}
+	var newAlbum album
+	c.BindJSON(&newAlbum)
+	album, err := storage.Update(id, newAlbum)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusOK, album)
+}
+
+func deleteAlbumsByID(c *gin.Context) {
+	id := c.Param("id")
+	err := storage.Delete(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusNoContent, album{})
 }
